505a85eb6637095f2fc7a0ed8ebb9994658ec936
Daffadon <daffaputranarendra9@gmail.com>
2023-03-10 19:26:13 +0700

    Program User Page

--- src/page/User/Program.jsx
+++ src/page/User/Program.jsx
@@ -6,7 +6,10 @@ import filter from "../../assets/icons/filter.svg"
 import CheckBox from "../../components/CheckBox/CheckBox";
 import { listposisi } from "../../container/Program/program";
 import { listProgram } from "../../container/Program/program";
+import { listCompany } from "../../container/LandingPage/Section4/listCompany";
 import { school } from "../../container/account/account";
+import loop from "../../assets/icons/loop.svg"
+import ProgramCarousel from "../../container/Program/ProgramCarousel";
 const Program = () => {
 	return (
 		<Layout>
@@ -24,11 +27,24 @@ const Program = () => {
 						<CheckBox item={listProgram} />
 						<p className={`${fontType["h3"]} mt-5`}>Tingkat</p>
 						<CheckBox item={["D3", "S1"]} />
-					<p className={`${fontType["h3"]} mt-5`}>Pendidikan Terakhir</p>
-					<CheckBox item={school} />
+						<p className={`${fontType["h3"]} mt-5`}>Pendidikan Terakhir</p>
+						<CheckBox item={school} />
 					</div>
 				</div>
-				<div className="w-3/4">
+				<div className="w-3/4 rounded-2xl ">
+					<div className="flex justify-center items-center">
+						<img src={loop} className="relative left-10	" />
+						<input
+							type="text"
+							className="w-full rounded-full px-16 border-none"
+							placeholder="Search" />
+					</div>
+					<div className="mt-10 pl-10">
+						<p className={`${fontType["h1"]} mb-5`}>Teknologi</p>
+						<ProgramCarousel slide={listCompany} />
+						<p className={`${fontType["h1"]} mb-5`}>Kesehatan</p>
+						<ProgramCarousel slide={listCompany} />
+					</div>
 				</div>
 			</div>
 		</Layout >
